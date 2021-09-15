25943537f160cb20de8e3c8f0b6a90d74c2af2ec
Sulfano Agus Fikri <sulfano.fikri@flash-coffee.com>
2021-09-16 00:40:55 +0700

    fix

--- app/BLoC/Web/EventOrder/GetEventPrice.php
+++ app/BLoC/Web/EventOrder/GetEventPrice.php
@@ -20,7 +20,7 @@ class GetEventPrice extends Retrieval
         $event_category = $parameters->get('category_event');
 
         $archery_event_price__normal_query = "
-            SELECT A.*, B.price,
+            SELECT A.*, B.price, B.id as registration_fee_id,
                 B.start_date as early_bird_start_date,
                 B.end_date as early_bird_end_date,
                 B.registration_type_id
@@ -34,7 +34,7 @@ class GetEventPrice extends Retrieval
         ]);
 
         $archery_event_price__early_bird_query = "
-            SELECT A.*, B.price,
+            SELECT A.*, B.price, B.id as registration_fee_id,
                 B.start_date as early_bird_start_date,
                 B.end_date as early_bird_end_date,
                 B.registration_type_id
@@ -74,7 +74,7 @@ class GetEventPrice extends Retrieval
             $team_category_id = $event_category['team_category_id'];
 
             $normal_price_result = collect($archery_event_price_normal_results)->first();
-            $normal_price_for_category = ArcheryEventRegistrationFeePerCategory::where('event_registration_fee_id', $normal_price_result->id)->where('team_category_id', $team_category_id)->first();
+            $normal_price_for_category = ArcheryEventRegistrationFeePerCategory::where('event_registration_fee_id', $normal_price_result->registration_fee_id)->where('team_category_id', $team_category_id)->first();
             if (!is_null($normal_price_for_category)) {
                 $total_price = $normal_price_for_category->price;
             }
@@ -82,7 +82,7 @@ class GetEventPrice extends Retrieval
             if (count($archery_event_price_early_bird_results) > 0) {
                 $early_bird_price_result = collect($archery_event_price_early_bird_results)->first();
                 if (!is_null($early_bird_price_result)) {
-                    $early_bird_price_for_category = ArcheryEventRegistrationFeePerCategory::where('event_registration_fee_id', $early_bird_price_result->id)->where('team_category_id', $team_category_id)->first();
+                    $early_bird_price_for_category = ArcheryEventRegistrationFeePerCategory::where('event_registration_fee_id', $early_bird_price_result->registration_fee_id)->where('team_category_id', $team_category_id)->first();
 
                     $early_bird_start_date = $early_bird_price_result->early_bird_start_date;
                     $early_bird_end_date = $early_bird_price_result->early_bird_end_date;
