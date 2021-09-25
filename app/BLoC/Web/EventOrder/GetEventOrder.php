f73f2ac7023a71fdf49cdf5a764aa032be28b0aa
surya darma saqti <surya@stockbit.com>
2021-09-25 09:54:57 +0700

    update

--- app/BLoC/Web/EventOrder/GetEventOrder.php
+++ app/BLoC/Web/EventOrder/GetEventOrder.php
@@ -26,6 +26,19 @@ class GetEventOrder extends Retrieval
             $transaction_info = PaymentGateWay::transactionLogPaymentInfo($participant->transaction_log_id);
             $participant_members = ArcheryEventParticipantMember::where("archery_event_participant_id", $participant->id)->get();
             $participant["members"] = $participant_members;
+            
+            $flat_categorie = $archery_event->flatCategories;
+            $category_label = $participant->team_category_id."-".$participant->age_category_id."-".$participant->competition_category_id."-".$participant->distance_id."m";
+            foreach ($flat_categorie as $key => $value) {
+                if($value->age_category_id == $participant->age_category_id
+                && $value->competition_category_id == $participant->competition_category_id
+                && $value->team_category_id == $participant->team_category_id
+                && $value->distance_id == $participant->distance_id
+                ){
+                    $category_label = $value->archery_event_category_label;
+                }
+            }
+            $participant->category_label = $category_label;
             $output[] = [
                 "archery_event" => $archery_event,
                 "participant" => $participant,
