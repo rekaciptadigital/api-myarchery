ed87d232d392f867e900eb86421d1239ac24dbe6
Mahfuzon Akhiar <61320778+mahfuzon@users.noreply.github.com>
2021-12-31 14:09:17 +0700

    membuat foreign key jadi objek data pada model ArcheryClub

--- app/Models/ArcheryClub.php
+++ app/Models/ArcheryClub.php
@@ -9,4 +9,15 @@ class ArcheryClub extends Model
     protected $table = 'archery_clubs';
     protected $primaryKey = 'id';
     protected $fillable = ['name', 'place_name', 'province', 'city', 'logo', 'address', 'description', 'banner'];
+    protected $appends = ['detail_province', 'detail_city'];
+
+    public function getDetailProvinceAttribute()
+    {
+        return $this->attributes['detail_province'] = Provinces::find($this->province);
+    }
+
+    public function getDetailCityAttribute()
+    {
+        return $this->attributes['detail_city'] = City::find($this->city);
+    }
 }
