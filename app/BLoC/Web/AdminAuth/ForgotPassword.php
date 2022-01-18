f155ff447a99ea3e2aa4c18e93d070b39943842a
propaganisa115 <iisanisajob@gmail.com>
2022-01-18 11:08:33 +0700

    forgot admin

--- app/BLoC/Web/AdminAuth/ForgotPassword.php
+++ app/BLoC/Web/AdminAuth/ForgotPassword.php
@@ -2,18 +2,48 @@
 
 namespace App\BLoC\Web\AdminAuth;
 
-use DAI\Utils\Abstracts\Transactional;
+use DAI\Utils\Abstracts\Retrieval;
+use DAI\Utils\Exceptions\BLoCException;
+use DAI\Utils\Helpers\BLoC;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redis;
+use App\Libraries\ForgetPassword;
+use App\Models\Admin;
 
-class ForgotPassword extends Transactional
+class ForgotPassword extends Retrieval
 {
     public function getDescription()
     {
-        return "Forgot Password";
+        return "";
     }
 
     protected function process($parameters)
     {
-        return $parameters;
+        $admin = Admin::where('email', $parameters->get('email'))->first();
+        if(!$admin) throw new BLoCException("Email tidak ditemukan");
+
+        $key = "email:verify:code:" . $admin->email;
+        $isKeyExist =  Redis::lrange($key, 0, -1);
+        $isKeyExp = Redis::ttl($key);
+
+        if($isKeyExist) {
+            $maxCount= count($isKeyExist);
+            //max try per day 3 times
+            if($maxCount>=3){
+                //minus means expired
+                if($isKeyExp<=0){
+                    Redis::del($key);
+                }
+                throw new BLoCException("Anda sudah mencoba forgot password 3x hari ini, coba lagi di jam berikutnya");
+            }else{
+                $code=$this->pushKey($admin,$key);
+            }
+        } else {
+            $code=$this->pushKey($admin,$key);
+        }
+        
+        $send_email = ForgetPassword::setEmail($admin->email)->setName($admin->name)->setCode($code)->sendMail();
+        return ["code" => 1, "msg" => "Kode sudah dikirim ke alamat email anda"];
     }
 
     protected function validation($parameters)
@@ -22,4 +52,5 @@ class ForgotPassword extends Transactional
             'email' => 'required',
         ];
     }
+
 }
--- app/Libraries/ForgetPassword.php
+++ app/Libraries/ForgetPassword.php
@@ -3,6 +3,9 @@
 namespace App\Libraries;
 use App\Jobs\ForgotPasswordEmailJob;
 use Queue;
+use Illuminate\Support\Facades\Redis;
+use DAI\Utils\Exceptions\BLoCException;
+
 
 class ForgetPassword
 {
@@ -42,4 +45,39 @@ class ForgetPassword
         ];
         return Queue::push(new ForgotPasswordEmailJob($data));
     }
+
+    public static function getCode($key,$admin)
+    {
+        $isKeyExist =  Redis::lrange($key, 0, -1);
+        $isKeyExp = Redis::ttl($key);
+
+        if($isKeyExist) {
+            $maxCount= count($isKeyExist);
+            //max try per day 3 times
+            if($maxCount>=3){
+                //minus means expired
+                if($isKeyExp<=0){
+                    Redis::del($key);
+                }
+                throw new BLoCException("Anda sudah mencoba forgot password 3x hari ini, coba lagi di jam berikutnya");
+            }else{
+                $code=self::pushKey($admin,$key);
+            }
+        } else {
+            $code=self::pushKey($admin,$key);
+        }
+
+        return $code;
+    }
+
+    public static function pushKey($admin,$key)
+    {
+            $code = substr(str_shuffle('1234567890'),0,5);
+            $value = ["admin_id" => $admin->id, "code" => $code];
+            $set = Redis::rpush($key, json_encode($value));
+            $set = Redis::expire($key, 3600);
+
+            return $code;
+    }
+
 }
