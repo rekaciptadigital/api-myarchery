c8ccaacf28cbe6c49c0ba43f7e23bf9108b649ba
propaganisa115 <iisanisajob@gmail.com>
2022-01-27 10:41:29 +0700

    pembaharuan event get all event, dan get event general

--- app/BLoC/Web/ArcheryEvent/GetArcheryEventGlobal.php
+++ app/BLoC/Web/ArcheryEvent/GetArcheryEventGlobal.php
@@ -20,10 +20,16 @@ class GetArcheryEventGlobal extends Retrieval
         $limit = !empty($parameters->get('limit')) ? $parameters->get('limit') : 1;
         $page = $parameters->get('page');
         $offset = ($page - 1) * $limit;
+        $event_name = $parameters->get('event_name');
 
         $archery_event = ArcheryEvent::select('*', DB::raw("if(now()>event_end_datetime,'selesai',if(now()<event_start_datetime,'akan berlangsung',if(now()> event_start_datetime && now()< event_end_datetime,'sedang berlangsung','false'))) as acara "))
                          ->orderBy('event_end_datetime', 'desc')
-                         ->where('status',1);
+                         ->where('status',1)
+                         ->where(function ($query) use ($event_name){
+                            if(!empty($event_name)){
+                                $query->where('archery_events.event_name', 'like', '%'.$event_name.'%');
+                            }
+                         });
 
         $total_page = ceil($archery_event->count() / $limit);
         $data = $archery_event->limit($limit)->offset($offset)->get();
