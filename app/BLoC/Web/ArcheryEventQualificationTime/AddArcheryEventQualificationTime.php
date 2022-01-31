a540fa940bfa23c15b5474338e4ed32fe3b4f27a
propaganisa115 <iisanisajob@gmail.com>
2022-01-31 09:57:26 +0700

    pemabharuan event set qualification time

--- app/BLoC/Web/ArcheryEventQualificationTime/AddArcheryEventQualificationTime.php
+++ app/BLoC/Web/ArcheryEventQualificationTime/AddArcheryEventQualificationTime.php
@@ -18,11 +18,21 @@ class AddArcheryEventQualificationTime extends Transactional
         $admin = Auth::user();
         $qualification_times = $parameters->get('qualification_time', []);
         foreach ($qualification_times as $qualification_time) {
-            $archery_event_qualification_time = new ArcheryEventQualificationTime();
-            $archery_event_qualification_time->category_detail_id = $qualification_time['category_detail_id'];
-            $archery_event_qualification_time->event_start_datetime =  $qualification_time['event_start_datetime'];
-            $archery_event_qualification_time->event_end_datetime =  $qualification_time['event_end_datetime'];
-            $archery_event_qualification_time->save();
+            $category_detail_id=$qualification_time['category_detail_id'];
+
+            $archery_event_qualification_time = ArcheryEventQualificationTime::where('category_detail_id', $category_detail_id)->first();
+            if (!$archery_event_qualification_time) {
+                $archery_event_qualification_time = new ArcheryEventQualificationTime();
+                $archery_event_qualification_time->category_detail_id = $qualification_time['category_detail_id'];
+                $archery_event_qualification_time->event_start_datetime =  $qualification_time['event_start_datetime'];
+                $archery_event_qualification_time->event_end_datetime =  $qualification_time['event_end_datetime'];
+                $archery_event_qualification_time->save();
+            }else{
+                $archery_event_qualification_time->category_detail_id = $qualification_time['category_detail_id'];
+                $archery_event_qualification_time->event_start_datetime =  $qualification_time['event_start_datetime'];
+                $archery_event_qualification_time->event_end_datetime =  $qualification_time['event_end_datetime'];
+                $archery_event_qualification_time->save();
+            }
         }
         
         return $archery_event_qualification_time;
