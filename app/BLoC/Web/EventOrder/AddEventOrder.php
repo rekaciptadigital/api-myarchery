17a75a9a0f473bcffd2543fbd3ad2b748fb617d2
mahfuzon <mahfuzon0@gmail.com>
2022-02-10 13:29:58 +0700

    1. menambahkan validasi pada update peserta hanya bisa melakukan update ketika j-24 2. menambahkan validasi pada update peserta tidak bisa melakukan update jika telah lewat event 3. untuk update individu tidak dapat melakukan ubah club jika pada club tujuan telah terdaftar di category yang sama dengan club sebelumnya 4. validasi jika sudah daftar tim di category yang satu grup dengan category individu maka tidak bisa ubah club 5. menghapus index unique untuk participant_member dan category pada table temporary 6. update peserta beregu done

--- app/BLoC/Web/EventOrder/AddEventOrder.php
+++ app/BLoC/Web/EventOrder/AddEventOrder.php
@@ -106,7 +106,7 @@ class AddEventOrder extends Transactional
             }
             // cek apakah usia user memenuhi syarat categori event
             if ($user->age > $event_category_detail->max_age) {
-                throw new BLoCException("tidak memenuhi syarat usia, syarat maksimal usia adalah" . $event_category_detail->max_gae." tahun");
+                throw new BLoCException("tidak memenuhi syarat usia, syarat maksimal usia adalah" . $event_category_detail->max_gae . " tahun");
             }
         }
 
@@ -117,21 +117,21 @@ class AddEventOrder extends Transactional
             }
             // cek apakah usia user memenuhi syarat categori event
             if ($user->age < $event_category_detail->min_age) {
-                throw new BLoCException("tidak memenuhi syarat usia, minimal usia adalah " . $event_category_detail->min_age." tahun");
+                throw new BLoCException("tidak memenuhi syarat usia, minimal usia adalah " . $event_category_detail->min_age . " tahun");
             }
         }
 
         $gender_category = $event_category_detail->gender_category;
         if ($user->gender != $gender_category) {
-            if(empty($user->gender))
+            if (empty($user->gender))
                 throw new BLoCException('silahkan set gender terlebih dahulu, kamu bisa update gender di halaman update profile :) ');
-            
+
             throw new BLoCException('oops.. kategori ini  hanya untuk gender ' . $gender_category);
         }
 
         // cek apakah user telah pernah mendaftar di categori tersebut
         $isExist = ArcheryEventParticipant::where('event_category_id', $event_category_detail->id)
-            ->where('user_id', $user->id)->first();
+            ->where('user_id', $user->id)->where('club_id', $club_member->club_id)->first();
         if ($isExist) {
             if ($isExist->status == 1) {
                 throw new BLoCException("event dengan kategori ini sudah di ikuti");
@@ -141,7 +141,7 @@ class AddEventOrder extends Transactional
                 if ($isExist_transaction_log->status == 4 && $isExist_transaction_log->expired_time > time()) {
                     throw new BLoCException("transaksi dengan kategory ini sudah pernah dilakukan, silahkan selesaikan pembayaran");
                 }
-            } 
+            }
         }
 
         // insert data participant
@@ -271,15 +271,13 @@ class AddEventOrder extends Transactional
                 ->join('transaction_logs', 'transaction_logs.id', '=', 'archery_event_participants.transaction_log_id')
                 ->where('temporrary_members.participant_member_id', $participant_member_old->id)
                 ->where('temporrary_members.event_category_id', $event_category_detail->id)
-                ->get(['transaction_logs.*'])->first();
+                ->get(['transaction_logs.*']);
 
-            if ($temporary) {
-                if ($temporary->status == 4 && $temporary->expired_time > time()) {
-                    throw new BLoCException("user dengan email " . $user_register->email . " telah didaftarkan pada category ini sebelumnya");
-                } elseif ($temporary->status == 2) {
-                    throw new BLoCException("order has expired please order again");
-                } elseif ($temporary->status == 1) {
-                    throw new BLoCException("user with email " . $user_register->email . " already join this category");
+            if ($temporary->count() > 0) {
+                foreach ($temporary as $t) {
+                    if ($t->status == 4 && $t->expired_time > time()) {
+                        throw new BLoCException("user dengan email " . $user_register->email . " telah didaftarkan pada category ini sebelumnya");
+                    }
                 }
             }
             array_push($participant_member_id, $participant_member_old);
