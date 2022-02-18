77c5d1414cef6a843c110cd1e84b4c86d84022d0
suryadarmasaqti19 <suryadarmasaqti19@gmail.com>
2022-02-19 02:30:20 +0700

    fix payment

--- app/Libraries/PaymentGateWay.php
+++ app/Libraries/PaymentGateWay.php
@@ -176,8 +176,9 @@ class PaymentGateWay
         $status = 3;
 
         $transaction_log = TransactionLog::where("order_id", $order_id)->first();
+        print_r($transaction_log);
         if (!$transaction_log || $transaction_log->status == 1) {
-            return false;
+            // return false;
         }
         if ($transaction == 'settlement') {
             $status = 1;
