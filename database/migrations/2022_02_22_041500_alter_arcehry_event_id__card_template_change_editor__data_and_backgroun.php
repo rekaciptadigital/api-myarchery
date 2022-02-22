1c50a3fa42f0295c24197e085f7c781c30b31128
propaganisa115 <iisanisajob@gmail.com>
2022-02-22 07:03:15 +0700

    perbaharui

---
+++ database/migrations/2022_02_22_041500_alter_arcehry_event_id__card_template_change_editor__data_and_backgroun.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterArcehryEventIdCardTemplateChangeEditorDataAndBackgroun extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('archery_event_idcard_templates', function (Blueprint $table) {
+            $table->longText('editor_data')->nullable()->change();
+            $table->longText('background')->nullable()->change();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
