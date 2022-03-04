c76874a9dc191d7e0dc8855d17eadcf73bc9cd91
suryadarmasaqti19 <suryadarmasaqti19@gmail.com>
2022-03-04 16:14:43 +0700

    update scoring

--- app/Models/ArcherySeriesUserPoint.php
+++ app/Models/ArcherySeriesUserPoint.php
@@ -19,19 +19,19 @@ class ArcherySeriesUserPoint extends Model
     protected $guarded = ['id'];
 
     protected function setPoint($member_id,$type,$pos){
+        $member = ArcheryEventParticipantMember::find($member_id);
+        if (!$member) return false;
+
+        $category_id = $participant->event_category_id;
         $category = ArcheryEventCategoryDetail::find($category_id);
         if (!$category) return false;
 
         $event_serie = ArcheryEventSerie::where("event_id", $category->event_id)->first();
         if (!$event_serie) return false;
 
-        $member = ArcheryEventParticipantMember::find($member_id);
-        if (!$member) return false;
-
         $participant = ArcheryEventParticipant::find($member->archery_event_participant_id);
         if(!$participant) return false;
 
-        $category_id = $participant->event_category_id;
         $user_id = $participant->user_id;
         $archerySeriesCategory = ArcherySeriesCategory::where("age_category_id", $category->age_category_id)
             ->where("competition_category_id", $category->competition_category_id)
