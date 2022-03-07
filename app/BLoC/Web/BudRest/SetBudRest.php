f6be214436ae4bd123d67755df24eb9d3c806072
mahfuzon <mahfuzon0@gmail.com>
2022-03-07 23:23:07 +0700

    nambahin validasi pada set elimmination, set bantalan, set jadwal

--- app/BLoC/Web/BudRest/SetBudRest.php
+++ app/BLoC/Web/BudRest/SetBudRest.php
@@ -24,11 +24,18 @@ class SetBudRest extends Transactional
     protected function process($parameters)
     {
         $admin = Auth::user();
-        $event_id=$parameters->get('event_id');
+        $event_id = $parameters->get('event_id');
 
         $event = ArcheryEvent::find($event_id);
         if (!$event) {
             throw new BLoCException('event tidak ditemukan');
+        }
+
+        $carbon_event_end_datetime = Carbon::parse($event->event_end_datetime);
+        $new_format_event_end_datetime = Carbon::create($carbon_event_end_datetime->year, $carbon_event_end_datetime->month, $carbon_event_end_datetime->day, 0, 0, 0);
+
+        if ($new_format_event_end_datetime < Carbon::today()) {
+            throw new BLoCException('event telah selesai');
         }
 
         if ($event->admin_id != $admin->id) {
@@ -36,31 +43,31 @@ class SetBudRest extends Transactional
         }
 
         $datas = $parameters->get('event_category', []);
-        
+
         foreach ($datas as $data) {
             $check_category = ArcheryEventCategoryDetail::select('archery_master_team_categories.type')
-                                ->leftJoin('archery_master_team_categories','archery_master_team_categories.id','archery_event_category_details.team_category_id')
-                                ->where('archery_event_category_details.id', $data['archery_event_category_id'])->where('event_id', $event_id)->first();
-            
+                ->leftJoin('archery_master_team_categories', 'archery_master_team_categories.id', 'archery_event_category_details.team_category_id')
+                ->where('archery_event_category_details.id', $data['archery_event_category_id'])->where('event_id', $event_id)->first();
+
             if (!$check_category) {
                 throw new BLoCException('category detail id tidak ditemukan');
             }
 
-            if ($check_category->type != 'Individual' ) {
+            if ($check_category->type != 'Individual') {
                 throw new BLoCException("tipe category detail id harus tipe individual");
             }
 
-            if ($data['bud_rest_start'] > $data['bud_rest_end'] || $data['bud_rest_end'] < $data['bud_rest_start']  ) {
+            if ($data['bud_rest_start'] > $data['bud_rest_end'] || $data['bud_rest_end'] < $data['bud_rest_start']) {
                 throw new BLoCException("bud rest start tidak boleh lebih besar dari bud rest end, begitu pula sebaliknya");
             }
 
-            if ($data['target_face'] > 4 ) { //4 adalah max ukuran bantalan
+            if ($data['target_face'] > 4) { //4 adalah max ukuran bantalan
                 throw new BLoCException("target face tidak boleh lebih dari 4");
             }
 
-            $budrest= Budrest::where('archery_event_category_id',$data['archery_event_category_id'])->first();
+            $budrest = Budrest::where('archery_event_category_id', $data['archery_event_category_id'])->first();
 
-            if(!$budrest){
+            if (!$budrest) {
                 $budrest = new Budrest();
                 $budrest->archery_event_category_id = $data['archery_event_category_id'];
                 $budrest->bud_rest_start =  $data['bud_rest_start'];
@@ -68,18 +75,18 @@ class SetBudRest extends Transactional
                 $budrest->target_face =  $data['target_face'];
                 $budrest->type =  $data['type'];
                 $budrest->save();
-            }else{
-                $check_qualification_time= ArcheryEventQualificationTime::where('category_detail_id',$data['archery_event_category_id'])->first();
+            } else {
+                $check_qualification_time = ArcheryEventQualificationTime::where('category_detail_id', $data['archery_event_category_id'])->first();
 
                 $now = Carbon::now();
 
-                if (is_null($check_qualification_time->event_start_datetime)){
+                if (is_null($check_qualification_time->event_start_datetime)) {
                     throw new BLoCException("set jadwal kualifikasi terlebih dahulu ");
                 }
 
                 // if (strtotime($check_qualification_time->event_start_datetime) < strtotime('now')){
                 //     throw new BLoCException("tidak bisa update data karna sudah lewat dari tanggal mulai qualifikasi");
-                    
+
                 // }
 
                 $budrest->archery_event_category_id = $data['archery_event_category_id'];
@@ -90,7 +97,7 @@ class SetBudRest extends Transactional
                 $budrest->save();
             }
             $key = env("REDIS_KEY_PREFIX") . ":qualification:score-sheet:updated";
-            Redis::hset($key,$data['archery_event_category_id'],$data['archery_event_category_id']);
+            Redis::hset($key, $data['archery_event_category_id'], $data['archery_event_category_id']);
         }
 
         return $budrest;
