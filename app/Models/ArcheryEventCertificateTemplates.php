83f8c296e1fe97a57a9e3d465abf4cb9f679f615
suryadarmasaqti19 <suryadarmasaqti19@gmail.com>
2022-03-16 09:45:46 +0700

    update certificate

--- app/Models/ArcheryEventCertificateTemplates.php
+++ app/Models/ArcheryEventCertificateTemplates.php
@@ -34,9 +34,9 @@ class ArcheryEventCertificateTemplates extends Model
     "1" => "Peserta",
     "2" => "Pemenang Eliminasi",
     "3" => "Eliminasi",
-    "4" => "Pemenang Kualifikasi",
-    "5" => "Pemenang Kualifikasi Beregu",
-    "6" => "Pemenang Kualifikasi Beregu Campuran",
+    "4" => "Kualifikasi",
+    "5" => "Kualifikasi Beregu",
+    "6" => "Kualifikasi Beregu Campuran",
   ];
 
   public static function getTypeCertificate()
