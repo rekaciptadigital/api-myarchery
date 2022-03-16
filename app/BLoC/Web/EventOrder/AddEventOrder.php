c2d2a9792e37edc3c4bc100f4d9e66864b5e7e0b
mahfuzon <mahfuzon0@gmail.com>
2022-03-16 13:48:52 +0700

    selesai fitur refund untuk sementara

--- app/BLoC/Web/EventOrder/AddEventOrder.php
+++ app/BLoC/Web/EventOrder/AddEventOrder.php
@@ -68,7 +68,7 @@ class AddEventOrder extends Transactional
 
         // cek apakah event butuh verifikasi user atau tidak
         if ($event->need_verify == 1) {
-            if ($user->verify_status == 4 || $user->verify_status == 2) {
+            if ($user->verify_status == 4) {
                 throw new BLoCException("akun anda belum terverifikasi");
             }
         }
