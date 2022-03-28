7f89729cd13e0f7f6fa90b98d603046bb3fce1f8
iis4 <iis@tees.co.id>
2022-03-28 12:27:36 +0700

    penambahan age category

--- app/BLoC/Web/Member/ListMemberV2.php
+++ app/BLoC/Web/Member/ListMemberV2.php
@@ -29,7 +29,6 @@ class ListMemberV2 extends Retrieval
         $team_category_id = $parameters->get("team_category_id");
         $age_category_id = $parameters->get("age_category_id");
         $type = $parameters->get("type");
-        $name = $parameters->get("name");
 
         $event = ArcheryEvent::find($event_id);
         if (!$event) {
@@ -43,11 +42,6 @@ class ListMemberV2 extends Retrieval
         }else{
             $participant_query = ArcheryEventParticipant::where("event_id", $event_id)->where("type", "individual");
         }
-            
-        // filter by name
-        $participant_query->when($name, function ($query) use ($name) {
-            return $query->whereRaw("archery_event_participants.name LIKE ?", ["%" . $name . "%"]);
-        });
 
         // filter by competition_id
         $participant_query->when($competition_category_id, function ($query) use ($competition_category_id) {
@@ -70,6 +64,7 @@ class ListMemberV2 extends Retrieval
         });
 
         $participant_collection = $participant_query->orderBy('id', 'DESC')->get();
+
 
         $output = [];
         $detail_member = [];
@@ -117,6 +112,7 @@ class ListMemberV2 extends Retrieval
                 $detail_member["phone_number"] = $user->phone_number;
                 $detail_member["competition_category"] = $competition_category_label;
                 $detail_member["status_payment"] = $status_payment;
+                $detail_member["age_category"] = $participant->age_category_id;
 
                 array_push($output, $detail_member);
             }
