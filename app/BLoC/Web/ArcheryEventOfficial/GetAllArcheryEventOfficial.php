3d7b9f3d459ea3514f1b031206d1f57973a21095
mahfuzon <mahfuzon0@gmail.com>
2022-04-26 09:14:50 +0700

    done elimination template 32

--- app/BLoC/Web/ArcheryEventOfficial/GetAllArcheryEventOfficial.php
+++ app/BLoC/Web/ArcheryEventOfficial/GetAllArcheryEventOfficial.php
@@ -24,8 +24,8 @@ class GetAllArcheryEventOfficial extends Retrieval
     protected function process($parameters)
     {
         $admin = Auth::user();
-        $name =$parameters->get('name');
-        
+        $name = $parameters->get('name');
+
         $event = ArcheryEvent::find($parameters->get('event_id'));
         if (!$event) {
             throw new BLoCException("event not found");
@@ -34,40 +34,41 @@ class GetAllArcheryEventOfficial extends Retrieval
         //hitung jumlah tersedia disini
         $archery_event_official_detail =  ArcheryEventOfficialDetail::where('event_id', $parameters->get('event_id'))->first();
 
-        if($archery_event_official_detail){
-            
-            if($archery_event_official_detail->individual_quota !=0){
-                $quota_total= $archery_event_official_detail->individual_quota;
-            }else{
-                $quota= $archery_event_official_detail->club_quota;
-                $count = ArcheryEventOfficial::count(DB::raw('DISTINCT club_id'));
-                $quota_total=$quota*$count;
+        if ($archery_event_official_detail) {
 
+            if ($archery_event_official_detail->individual_quota != 0) {
+                $quota_total = $archery_event_official_detail->individual_quota;
+            } else {
+                $quota = $archery_event_official_detail->club_quota;
+                $count = ArcheryEventOfficial::count(DB::raw('DISTINCT club_id'));
+                $quota_total = $quota * $count;
             }
         }
-        
+
         $official_count = ArcheryEventOfficial::countEventOfficialBooking($archery_event_official_detail->id);
 
-        $official_member = ArcheryEventOfficial::select('users.name as user_name','archery_clubs.name as club_name','users.email as email', 'users.phone_number as phone_number')
-                            ->where('archery_event_official_detail.event_id', $parameters->get('event_id'))
-                            ->leftJoin('archery_clubs','archery_clubs.id','=','archery_event_official.club_id')
-                            ->leftJoin('users','users.id','=','archery_event_official.user_id')
-                            ->leftJoin('archery_event_official_detail','archery_event_official_detail.id','=','archery_event_official.event_official_detail_id')
-                            ->where(function ($query) use ($name) {
-                                if(!empty($name)){
-                                    $query->whereRaw("users.name LIKE ?", ["%" . $name . "%"]);
-                                }
-                            })
-                            ->get();
+        $official_member = ArcheryEventOfficial::select('users.name as user_name', 'archery_clubs.name as club_name', 'users.email as email', 'users.phone_number as phone_number')
+            ->where('archery_event_official_detail.event_id', $parameters->get('event_id'))
+            ->leftJoin('archery_clubs', 'archery_clubs.id', '=', 'archery_event_official.club_id')
+            ->leftJoin('users', 'users.id', '=', 'archery_event_official.user_id')
+            ->leftJoin('archery_event_official_detail', 'archery_event_official_detail.id', '=', 'archery_event_official.event_official_detail_id')
+            ->where(function ($query) use ($name) {
+                if (!empty($name)) {
+                    $query->whereRaw("users.name LIKE ?", ["%" . $name . "%"]);
+                }
+            })
+            ->get();
 
         if ($official_member->isEmpty()) {
             throw new BLoCException("data not found");
         }
 
-        $data=["kuota_event"=>$quota_total,
-            "sisa_kuota"=>$quota_total-$official_count,
-            "member"=>$official_member];
-        
+        $data = [
+            "kuota_event" => $quota_total,
+            "sisa_kuota" => $quota_total - $official_count,
+            "member" => $official_member
+        ];
+
         return $data;
     }
 
