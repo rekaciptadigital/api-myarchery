01f66ef23318d977601cf586a46324312f67e101
mahfuzon <mahfuzon0@gmail.com>
2022-05-27 07:58:21 +0700

    belum fix admin total

--- app/BLoC/Web/ArcheryScoring/AddParticipantMemberScore.php
+++ app/BLoC/Web/ArcheryScoring/AddParticipantMemberScore.php
@@ -44,7 +44,7 @@ class AddParticipantMemberScore extends Transactional
             return $this->addScoringQualification($parameters);
         }
 
-        if ($parameters->type == 2) {
+        if ($type == 2) {
             return $this->addScoringElimination($parameters);
         }
     }
@@ -193,6 +193,8 @@ class AddParticipantMemberScore extends Transactional
         if ($get_elimination->elimination_scoring_type == 2) {
             $calculate = ArcheryScoring::calculateEliminationScoringTypeTotalFormat($members[0], $members[1], $save_permanent);
         }
+
+        // return $members[0]["scores"]["admin_total"];
         foreach ($get_member_match as $key => $value) //check valid members 
         {
             $participant_member_id = $value->member_id;
@@ -206,8 +208,25 @@ class AddParticipantMemberScore extends Transactional
             $item_id = $value->id;
             $participant_scoring = ArcheryScoring::where("type", 2)->where("item_id", $item_id)->first();
             if (!$participant_scoring) {
+                $admin_total = $total;
                 $participant_scoring = new ArcheryScoring;
+            } else {
+                $admin_total = $participant_scoring->admin_total;
+                if ($participant_scoring->total != $total) {
+                    $admin_total = $total;
+                }
+
+                if (isset($calculate[$participant_member_id]["scores"]["admin_total"])) {
+                    if ($calculate[$participant_member_id]["scores"]["admin_total"] != $admin_total) {
+                        $admin_total = $calculate[$participant_member_id]["scores"]["admin_total"];
+                    }
+
+                    if ($participant_scoring->total != $total) {
+                        $admin_total = $total;
+                    }
+                }
             }
+
             $participant_scoring->participant_member_id = $participant_member_id;
             $participant_scoring->total = $total;
             $participant_scoring->scoring_session = $session;
@@ -216,6 +235,7 @@ class AddParticipantMemberScore extends Transactional
             $participant_scoring->item_id = $item_id;
             $participant_scoring->scoring_log = \json_encode($value);
             $participant_scoring->scoring_detail = \json_encode($scoring);
+            $participant_scoring->admin_total = $admin_total;
             $participant_scoring->save();
             $elimination_match = ArcheryEventEliminationMatch::where("id", $value->id)->first();
             $elimination_match->result = $result;
