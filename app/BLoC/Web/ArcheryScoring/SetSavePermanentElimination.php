06a0fd7552513468e28f5e81b87343fc4455ab5f
mahfuzon <mahfuzon0@gmail.com>
2022-07-03 06:29:58 +0700

    perbaiki shoot off team

--- app/BLoC/Web/ArcheryScoring/SetSavePermanentElimination.php
+++ app/BLoC/Web/ArcheryScoring/SetSavePermanentElimination.php
@@ -259,26 +259,31 @@ class SetSavePermanentElimination extends Retrieval
         if ($get_member_match[1]->admin_total == $get_member_match[0]->admin_total) {
             $result_shot_of_1 = 0;
             foreach ($scoring_detail_1->extra_shot as $key => $value) {
+
+                if (gettype($value->score) == "string" && strtolower($value->score) == "x") {
+                    $result_shot_of_1 = $result_shot_of_1 + 11;
+                    continue;
+                }
+
                 if ($value->score == "" || $value->score == 0 || $value->score == "m") {
                     continue;
                 }
-                $shoot_off = $value->score;
-                if ($value->score == "x") {
-                    $shoot_off = 10.1;
-                }
-                $result_shot_of_1 = $result_shot_of_1 + $shoot_off;
+
+                $result_shot_of_1 = $result_shot_of_1 + $value->score;
             }
 
             $result_shot_of_2 = 0;
             foreach ($scoring_detail_2->extra_shot as $key => $value) {
+                if (gettype($value->score) == "string" && strtolower($value->score) == "x") {
+                    $result_shot_of_2 = $result_shot_of_2 + 11;
+                    continue;
+                }
+
                 if ($value->score == "" || $value->score == 0 || $value->score == "m") {
                     continue;
                 }
-                $shoot_off = $value->score;
-                if ($value->score == "x") {
-                    $shoot_off = 10.1;
-                }
-                $result_shot_of_2 = $result_shot_of_2 + $shoot_off;
+              
+                $result_shot_of_2 = $result_shot_of_2 + $value->score;
             }
 
             if ($result_shot_of_1 > $result_shot_of_2) {
