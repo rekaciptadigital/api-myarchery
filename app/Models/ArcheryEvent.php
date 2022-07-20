4522633e2602fcf2b2f5b9bd685fd349d7c08526
mahfuzon <mahfuzon0@gmail.com>
2022-07-20 07:08:22 +0700

    perbaiki tanggal

--- app/Models/ArcheryEvent.php
+++ app/Models/ArcheryEvent.php
@@ -18,7 +18,7 @@ class ArcheryEvent extends Model
         'detail_admin', 'more_information', "event_price", "location_date_event"
     ];
     protected $guarded = ['id'];
-    protected $dates = ['event_start_datetime', 'event_end_datetime'];
+    // protected $dates = ['event_start_datetime', 'event_end_datetime'];
 
     public function getEventPriceAttribute()
     {
