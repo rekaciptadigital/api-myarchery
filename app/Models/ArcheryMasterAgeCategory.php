5585a8f7b172172e20fb41bfc16350b71e06079c
mahfuzon <mahfuzon0@gmail.com>
2022-07-27 09:56:09 +0700

    nambahin penanda kalau id sebagai primary key

--- app/Models/ArcheryMasterAgeCategory.php
+++ app/Models/ArcheryMasterAgeCategory.php
@@ -9,4 +9,5 @@ class ArcheryMasterAgeCategory extends Model
     protected $table = 'archery_master_age_categories';
     public $incrementing = false;
     protected $keyType = 'string';
+    protected $primaryKey = 'id';
 }
