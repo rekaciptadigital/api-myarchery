9e38f78bf95dff8d98338e7198397ace6ebf34a8
mahfuzon <mahfuzon0@gmail.com>
2022-07-29 21:32:52 +0700

    membuat function khusus juga peserta cuma 1

--- app/Models/ArcheryScoringEliminationGroup.php
+++ app/Models/ArcheryScoringEliminationGroup.php
@@ -182,6 +182,36 @@ class ArcheryScoringEliminationGroup extends Model
         ];
     }
 
+    public static function calculateEliminationScoringTypeTotalFormatBye(array $scoring_1)
+    {
+        $scores = self::$elimination_scores_format_by_type[2];
+        $total_score_1 = 0;
+
+        foreach ($scores["shot"] as $k => $shot) {
+            $scoring_1_total_score_per_rambahan = 0;
+            foreach ($shot["score"] as $i => $s) {
+                $s1 = self::$score_value[$scoring_1["scores"]["shot"][$k]["score"][$i]];
+                $scoring_1_total_score_per_rambahan = $scoring_1_total_score_per_rambahan + $s1;
+            }
+
+            $total_score_1 = $total_score_1 + $scoring_1_total_score_per_rambahan;
+            $scoring_1["scores"]["shot"][$k]["total"] = $scoring_1_total_score_per_rambahan;
+        }
+
+
+        $scoring_1["scores"]["total"] = $total_score_1;
+
+
+        $scoring_1["scores"]["result"] = $total_score_1;
+
+
+        $scoring_1["scores"]["eliminationt_score_type"] = 2;
+
+        return [
+            $scoring_1["participant_id"] => $scoring_1,
+        ];
+    }
+
     public static function calculateEliminationScoringTypePointFormat(array $scoring_1, array $scoring_2, $save_permanent)
     {
         $scores = self::$elimination_scores_format_by_type[1];
@@ -313,6 +343,46 @@ class ArcheryScoringEliminationGroup extends Model
         return [
             $scoring_1["participant_id"] => $scoring_1,
             $scoring_2["participant_id"] => $scoring_2,
+        ];
+    }
+
+    public static function calculateEliminationScoringTypePointFormatbye(array $scoring_1)
+    {
+        $scores = self::$elimination_scores_format_by_type[1];
+        $total_point_1 = 0;
+        $total_score_1 = 0;
+        $win_1 = 0;
+
+        foreach ($scores["shot"] as $k => $shot) {
+            $scoring_1_total_score_per_rambahan = 0;
+            $point_1_per_rambahan = 0;
+            foreach ($shot["score"] as $i => $s) {
+                $s1 = self::$score_value[$scoring_1["scores"]["shot"][$k]["score"][$i]];
+                $scoring_1_total_score_per_rambahan = $scoring_1_total_score_per_rambahan + $s1;
+            }
+
+            
+
+            $total_score_1 = $total_score_1 + $scoring_1_total_score_per_rambahan;
+            $scoring_1["scores"]["shot"][$k]["total"] = $scoring_1_total_score_per_rambahan;
+
+            $total_point_1 = $total_point_1 + $point_1_per_rambahan;
+            $scoring_1["scores"]["shot"][$k]["point"] = $point_1_per_rambahan;
+        }
+
+
+
+        $scoring_1["scores"]["total"] = $total_score_1;
+
+
+        $scoring_1["scores"]["result"] = $total_point_1;
+
+
+        $scoring_1["scores"]["eliminationt_score_type"] = 1;
+
+
+        return [
+            $scoring_1["participant_id"] => $scoring_1,
         ];
     }
 }
