d919172cd2836a5407c0c3a57d3d0544fd60e025
mahfuzon <mahfuzon0@gmail.com>
2022-08-04 16:38:36 +0700

    Revert "hapus validate email"
    This reverts commit edcf5bc564e624f4199b15bb4dcc8184a81b27c3.

--- app/BLoC/App/UserAuth/UserRegister.php
+++ app/BLoC/App/UserAuth/UserRegister.php
@@ -68,6 +68,7 @@ class UserRegister extends Transactional
             'password' => 'required|string|min:6|confirmed',
             'date_of_birth' => 'required|date',
             'gender' => 'required|in:male,female',
+            'phone_number' => 'required'
         ];
     }
 }
