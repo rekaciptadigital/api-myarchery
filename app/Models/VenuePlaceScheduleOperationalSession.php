a6bac831b288f1871ac32a63e9b9b3c49a4270bf
fiyuang <fitrianggraini96@gmail.com>
2022-08-12 15:42:25 +0700

    update venue session setting

--- app/Models/VenuePlaceScheduleOperationalSession.php
+++ app/Models/VenuePlaceScheduleOperationalSession.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class VenuePlaceScheduleOperationalSession extends Model
 {
@@ -13,7 +14,8 @@ class VenuePlaceScheduleOperationalSession extends Model
 
     protected function getListScheduleOperationalSessionByPlaceId($place_id)
     {
-        $schedule_operationals = VenuePlaceScheduleOperational::where('place_id', $place_id)
+        $schedule_operationals = VenuePlaceScheduleOperational::select(DB::RAW('distinct day as day'), 'id')
+                                    ->where('place_id', $place_id)
                                     ->where('is_open', true)
                                     ->orderByRaw("FIELD(day, \"Senin\", \"Selasa\", \"Rabu\", \"Kamis\", \"Jumat\", \"Sabtu\", \"Minggu\")")
                                     ->get();
@@ -23,21 +25,25 @@ class VenuePlaceScheduleOperationalSession extends Model
         $session = [];
         foreach ($schedule_operationals as $operational) {
             $session_settings = VenuePlaceScheduleOperationalSession::where('schedule_operational_id', $operational->id)->get();
-            if (!$session_settings) {
-                return $schedule_operationals;
-            } else {
-                $result[$operational->day] = [];
-                foreach ($session_settings as $key) {
-                    $session['id'] = $key->id;
-                    $session['schedule_operational_id'] = $key->schedule_operational_id;
-                    $session['start_time'] = $key->start_time;
-                    $session['end_time'] = $key->end_time;
-                    $session['total_budrest'] = $key->total_budrest;
-                    $session['total_target'] = $key->total_target;
-                    $session['max_capacity'] = $key->max_capacity;
-                    array_push($result[$operational->day], $session);
+                $result[$operational->day]['id'] = $operational->id;
+
+                if (sizeof($session_settings) == 0) {
+                    $result[$operational->day]['data'] = [];
+                } else {
+                    $sessions = [];
+                    foreach ($session_settings as $key) {
+                        $session['id'] = $key->id;
+                        $session['schedule_operational_id'] = $key->schedule_operational_id;
+                        $session['start_time'] = $key->start_time;
+                        $session['end_time'] = $key->end_time;
+                        $session['total_budrest'] = $key->total_budrest;
+                        $session['total_target'] = $key->total_target;
+                        $session['max_capacity'] = $key->max_capacity;
+                        array_push($sessions, $session);
+                    }
+                    $result[$operational->day]['data'] = $sessions;
                 }
-            }
+                
         }
         return $result;
     }
