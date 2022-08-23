4c18057cbc5781d4cd6f6a1d963d2c78807b6576
suryadarmasaqti19 <suryadarmasaqti19@gmail.com>
2022-08-23 23:53:23 +0700

    score sheet

--- resources/views/template/score_sheet_qualification_group_by_budrest.blade.php
+++ resources/views/template/score_sheet_qualification_group_by_budrest.blade.php
@@ -47,7 +47,7 @@
                 <br>
                 <br>
                 <br>
-                <table style="height: 276px; width: 549px;" border="0" cellspacing="0" cellpadding="0">
+                <table style="height: 256px; width: 549px;" border="0" cellspacing="0" cellpadding="0">
                     <tbody>
                         <tr style="height: 30px;">
                             <td style="width: 150px; height: 86px; border-style: none; text-align: center;">
@@ -70,7 +70,7 @@
                     </tbody>
                 </table>
 
-                <table style="height: 276px; width: 549px;" border="0" cellspacing="0" cellpadding="0">
+                <table style="height: 246px; width: 549px;" border="0" cellspacing="0" cellpadding="0">
                     <tbody>
                         <tr style="height: 60px;text-align: left;float: left;">
                             <td
@@ -100,7 +100,7 @@
                     </tbody>
                 </table>
                 <br />
-                <table class="table-scorer" style="height: 276px; width: 549px;">
+                <table class="table-scorer" style="height: 246px; width: 549px;">
                     <thead>
                         <tr style="height: 42px;">
                             <th style="font-size: 11px; width: 17px; height: 42px; text-align: left;">
@@ -121,56 +121,55 @@
                     </thead>
                     <tbody>
                         <?php for ($i=0; $i < $total_stage; $i++) { ?>
-                            <tr style="background-color: #e3e2de;">
-                                <td class="border" style="height: 36px; width: 17px;" rowspan="2">{{$i+1}}</td>
-                                <td class="border" style="height: 30px; width: 20px;">&nbsp;</td>
+                            <tr <?php if(($i%2==0)){echo 'style="background-color: #e3e2de;"'; } ?>>
+                                <td class="border" style="height: 25px; width: 17px;" rowspan="2">{{$i+1}}</td>
+                                <td class="border" style="height: 25px; width: 20px;">&nbsp;</td>
                                 <?php if($total_shot_per_stage > 2){ ?>
-                                <td class="border" style="height: 30px; width: 20px;">&nbsp;</td>
+                                <td class="border" style="height: 25px; width: 20px;">&nbsp;</td>
                                 <?php }if($total_shot_per_stage > 4){ ?>
-                                <td class="border" style="height: 30px; width: 20px;">&nbsp;</td>
+                                <td class="border" style="height: 25px; width: 20px;">&nbsp;</td>
                                 <?php }if($total_shot_per_stage > 6){ ?>
-                                    <td class="border" style="height: 30px; width: 20px;">&nbsp;</td>
+                                    <td class="border" style="height: 25px; width: 20px;">&nbsp;</td>
                                 <?php } ?>
                                 <?php if($total_shot_per_stage > 8){ ?>
-                                    <td class="border" style="height: 30px; width: 20px;">&nbsp;</td>
+                                    <td class="border" style="height: 25px; width: 20px;">&nbsp;</td>
                                 <?php } ?>
-                                <td class="border" style="height: 30px; width: 50.2;">&nbsp;</td>
-                                <td class="border" style="height: 30px; width: 50.3px;" rowspan="2">&nbsp;</td>
-                                <td class="border" style="height: 30px; width: 50.01px;">&nbsp;</td>
-                                <td class="border" style="height: 30px; width: 50px;">&nbsp;</td>
+                                <td class="border" style="height: 25px; width: 50.2;">&nbsp;</td>
+                                <td class="border" style="height: 25px; width: 50.3px;" rowspan="2">&nbsp;</td>
+                                <td class="border" style="height: 25px; width: 50.01px;">&nbsp;</td>
+                                <td class="border" style="height: 25px; width: 50px;">&nbsp;</td>
                             </tr>
-                            <tr style="background-color: #e3e2de;">
-                                <td class="border" style="height: 30px; width: 20px;">&nbsp;</td>
+                            <tr <?php if(($i%2==0)){?> style="background-color: #e3e2de;" <?php } ?>>
+                                <td class="border" style="height: 25px; width: 20px;">&nbsp;</td>
                                 <?php if($total_shot_per_stage > 2){ ?>
-                                <td class="border" style="height: 30px; width: 20px;">&nbsp;</td>
+                                <td class="border" style="height: 25px; width: 20px;">&nbsp;</td>
                                 <?php }if($total_shot_per_stage > 4){ ?>
-                                    <td class="border" style="height: 30px; width: 20px;">&nbsp;</td>
+                                    <td class="border" style="height: 25px; width: 20px;">&nbsp;</td>
                                 <?php }if($total_shot_per_stage > 6){ ?>
-                                    <td class="border" style="height: 30px; width: 20px;">&nbsp;</td>
+                                    <td class="border" style="height: 25px; width: 20px;">&nbsp;</td>
                                 <?php } ?>
                                 <?php if($total_shot_per_stage > 8){ ?>
-                                    <td class="border" style="height: 30px; width: 20px;">&nbsp;</td>
+                                    <td class="border" style="height: 25px; width: 20px;">&nbsp;</td>
                                 <?php } ?>
-                                <td class="border" style="height: 30px; width: 50.2;">&nbsp;</td>
-                                <td class="border" style="height: 30px; width: 50.01px;">&nbsp;</td>
-                                <td class="border" style="height: 30px; width: 50px;">&nbsp;</td>
+                                <td class="border" style="height: 25px; width: 50.2;">&nbsp;</td>
+                                <td class="border" style="height: 25px; width: 50.01px;">&nbsp;</td>
+                                <td class="border" style="height: 25px; width: 50px;">&nbsp;</td>
                             </tr>
                         <?php } ?>
                         
                         <tr style="height: 60px;">
-                            <td class="border" style="width: 164px; height: 30px;" colspan="4">kode :
+                            <td class="border" style="width: 164px; height: 25px;" colspan="4">kode :
                                 {{ $m['code'] }}
                             </td>
-                            <td class="border" style="width: 50.2; height: 30px;"><strong>Total</strong></td>
-                            <td class="border" style="width: 50.3px; height: 30px;">&nbsp;</td>
-                            <td class="border" style="width: 50.3px; height: 30px;">&nbsp;</td>
-                            <td class="border" style="width: 50.3px; height: 30px;">&nbsp;</td>
+                            <td class="border" style="width: 50.2; height: 25px;"><strong>Total</strong></td>
+                            <td class="border" style="width: 50.3px; height: 25px;">&nbsp;</td>
+                            <td class="border" style="width: 50.3px; height: 25px;">&nbsp;</td>
+                            <td class="border" style="width: 50.3px; height: 25px;">&nbsp;</td>
                         </tr>
                     </tbody>
                 </table>
-                <br><br><br>
-                <br><br><br>
-                <table style="height: 276px; width: 100%;" border="0" cellspacing="0" cellpadding="0">
+                <br><br><br><br><br>
+                <table style="height: 246px; width: 100%;" border="0" cellspacing="0" cellpadding="0">
                     <tbody>
                         <tr style="height: 15px;">
                             <td style="border-top: 1pt solid black;width: 30%; height: 14px; text-align: center;">wasit
@@ -187,8 +186,7 @@
         <?php } ?>
     </table>
     <br><br><br>
-    <br><br><br>
-    <table style="height: 276px; width: 100%;" border="0" cellspacing="0" cellpadding="0">
+    <table style="height: 246px; width: 100%;" border="0" cellspacing="0" cellpadding="0">
         <tbody>
             <tr>
                 <td style="width: 23.1329%; height: 10px;">
