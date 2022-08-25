e99a9232e6dce4cc0dda58c9e9315dd2bfd22d54
mahfuzon <mahfuzon0@gmail.com>
2022-08-25 11:01:54 +0700

    belum done

--- database/migrations/2022_08_24_144511_add_field_rules_rating_club_to_category_table.php
+++ database/migrations/2022_08_24_144511_add_field_rules_rating_club_to_category_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFieldRatingFlagToCategoryDetail extends Migration
+class AddFieldRulesRatingClubToCategoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddFieldRatingFlagToCategoryDetail extends Migration
     public function up()
     {
         Schema::table('archery_event_category_details', function (Blueprint $table) {
-            $table->smallInteger("rating_flag")->default(1)->comment("1 untuk dimasukkan ke semua pemeringkatan, 2 untuk pemeringkatan khusus diluar semua pemeringkatan");
+            $table->integer("rules_rating_club")->default(1)->comment("1 untuk semua kategori, 2 untuk satuan khusus, 3 kategori gabungan");
         });
     }
 
@@ -26,7 +26,7 @@ class AddFieldRatingFlagToCategoryDetail extends Migration
     public function down()
     {
         Schema::table('archery_event_category_details', function (Blueprint $table) {
-            $table->dropColumn("rating_flag");
+            $table->dropColumn("rules_rating_club");
         });
     }
 }
