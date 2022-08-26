1397bfda88ec9d0ec10dc16dd52f7f2752afb949
fiyuang <fitrianggraini96@gmail.com>
2022-08-26 10:52:00 +0700

    add order venue product (update payment)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -56,6 +56,7 @@ use App\BLoC\App\UserAuth\ValidateAccoutVerification;
 use App\BLoC\App\Enterprise\GetDetailVenuePlace;
 use App\BLoC\App\Enterprise\GetListProductByVenuePlace;
 use App\BLoC\App\Enterprise\GetInfoOrderProduct;
+use App\BLoC\App\Enterprise\AddOrderVenueProduct;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -126,6 +127,7 @@ class AppServiceProvider extends ServiceProvider
         $this->registerService("getDetailVenuePlace", GetDetailVenuePlace::class);
         $this->registerService("getListProductByVenuePlace", GetListProductByVenuePlace::class);
         $this->registerService("getInfoOrderProduct", GetInfoOrderProduct::class);
+        $this->registerService("addOrderVenueProduct", AddOrderVenueProduct::class);
 
         // -------------------------------------------- End of Archery Enterprise Service --------------------------------------------- //
 
