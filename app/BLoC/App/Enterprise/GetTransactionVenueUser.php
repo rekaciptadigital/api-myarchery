72fd2247c0006c0a78d5a661ca359fbdf0c7db33
fiyuang <fitrianggraini96@gmail.com>
2022-08-31 09:01:34 +0700

    update transaction venue

--- app/BLoC/App/Enterprise/GetTransactionVenueUser.php
+++ app/BLoC/App/Enterprise/GetTransactionVenueUser.php
@@ -24,14 +24,15 @@ class GetTransactionVenueUser extends Retrieval
         $user = Auth::guard('app-api')->user();
         $status = $parameters->get('status');
 
-        $transactions = VenuePlaceProductOrder::select("venue_place_product_orders.*")
+        $transactions = VenuePlaceProductOrder::select("venue_place_product_orders.*", "venue_places.name as venue_name", "venue_place_products.product_name as product_name", "venue_place_products.total_each_rent_per_day as total_each_rent_per_day", "venue_place_products.base_product as base_product")
+                            ->join("venue_place_products", "venue_place_products.id", "=", "venue_place_product_orders.product_id")
+                            ->join("venue_places", "venue_places.id", "=", "venue_place_products.place_id")
                             ->join("transaction_logs", "transaction_logs.id", "=", "venue_place_product_orders.transaction_log_id")
                             ->where(function ($query) use ($status) {
                                 $query->where('transaction_logs.status', $status);
                             })
                             ->where('user_id', $user->id)
                             ->get();
-        
 
         return $transactions;
     }
