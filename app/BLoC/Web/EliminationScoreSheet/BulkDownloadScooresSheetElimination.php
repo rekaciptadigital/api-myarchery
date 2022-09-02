a6cb639069e38cd1d9b986335d2a4ab45046ddd3
mahfuzon <mahfuzon0@gmail.com>
2022-09-02 09:22:03 +0700

    done download score sheet individu

--- app/BLoC/Web/EliminationScoreSheet/BulkDownloadScooresSheetElimination.php
+++ app/BLoC/Web/EliminationScoreSheet/BulkDownloadScooresSheetElimination.php
@@ -2,6 +2,7 @@
 
 namespace App\BLoC\Web\EliminationScoreSheet;
 
+use App\Libraries\PdfLibrary;
 use App\Models\ArcheryClub;
 use App\Models\ArcheryEvent;
 use App\Models\ArcheryEventCategoryDetail;
@@ -74,7 +75,6 @@ class BulkDownloadScooresSheetElimination extends Retrieval
             throw new BLoCException("elimination not found");
         }
 
-        $match_index = 0;
         $list_match = [];
         $data_member = ArcheryEventEliminationMatch::where('event_elimination_id', $elimination_id)
             ->where('round', $round)
@@ -85,72 +85,90 @@ class BulkDownloadScooresSheetElimination extends Retrieval
         }
 
         foreach ($data_member as $dm) {
-            if ($dm->match != $match_index) {
-                $match_index = $dm->match;
-                $list_match[] = $dm;
-            }else{
-                $list_match[] = $dm;
-            }
-            return $list_match;
+            $list_match[$dm->match][$dm->index] = $dm;
         }
 
-        $string_code = "2-" . $data_member[0]->event_elimination_id . "-" . $data_member[0]->match . "-" . $data_member[0]->round;
-        $path = 'asset/score_sheet/' . $category_id  . '/';
-        if (!file_exists(public_path() . "/" . $path)) {
-            mkdir(public_path() . "/" . $path, 0777);
-        }
-        $qrCode = new QrCode($string_code);
+        $all = [];
+        foreach ($list_match as $value) {
+            $string_code = "2-" . $value[0]->event_elimination_id . "-" . $value[0]->match . "-" . $value[0]->round;
+            $path = 'asset/score_sheet/' . $category_id  . '/';
+            if (!file_exists(public_path() . "/" . $path)) {
+                
+                mkdir(public_path() . "/" . $path, 0777);
+                
+            }
+            $qrCode = new QrCode($string_code);
 
-        $output_qrcode = new Output\Png();
+            $output_qrcode = new Output\Png();
 
-        $qrCode_name_file = "qr_code_" . $string_code . ".png";
+            $qrCode_name_file = "qr_code_" . $string_code . ".png";
 
-        $full_path = $path . $qrCode_name_file;
+            $full_path = $path . $qrCode_name_file;
 
-        $data_qr_code =  $output_qrcode->output($qrCode,  100, [255, 255, 255], [0, 0, 0]);
+            $data_qr_code =  $output_qrcode->output($qrCode,  100, [255, 255, 255], [0, 0, 0]);
 
-        file_put_contents(public_path() . '/' . $full_path, $data_qr_code);
+            file_put_contents(public_path() . '/' . $full_path, $data_qr_code);
 
-        $data_get_qr_code = file_get_contents(public_path() . "/" . $full_path);
-        $base64 = 'data:image/png;base64,' . base64_encode($data_get_qr_code);
+            $data_get_qr_code = file_get_contents(public_path() . "/" . $full_path);
+            $base64 = 'data:image/png;base64,' . base64_encode($data_get_qr_code);
 
-        foreach ($data_member as $data) {
-            $name = "";
-            $rank = "";
-            $club = "";
-            $elimination_member = ArcheryEventEliminationMember::find($data->elimination_member_id);
-            if ($elimination_member) {
-                $participant_member_id = $elimination_member->member_id;
+            $result = [];
+            foreach ($value as $v) {
+                $name = "";
+                $rank = "";
+                $club = "";
+                $elimination_member = ArcheryEventEliminationMember::find($v->elimination_member_id);
+                if ($elimination_member) {
+                    $participant_member_id = $elimination_member->member_id;
 
-                $detail_member = ArcheryEventParticipantMember::select(
-                    'archery_event_participant_members.name as name',
-                    'archery_clubs.name as club_name',
-                    'archery_event_participants.id as participant_id',
-                    'archery_event_participants.user_id as user_id',
-                    'archery_event_participants.event_id'
-                )
-                    ->where('archery_event_participant_members.id', $participant_member_id)
-                    ->leftJoin('archery_event_participants', 'archery_event_participants.id', 'archery_event_participant_members.archery_event_participant_id')
-                    ->leftJoin('archery_clubs', 'archery_clubs.id', 'archery_event_participants.club_id')
-                    ->first();
-                $name = $detail_member['name'];
-                $rank = $elimination_member->elimination_ranked;
-                $club = $detail_member['club_name'] ? $detail_member['club_name'] : "-";
+                    $detail_member = ArcheryEventParticipantMember::select(
+                        'archery_event_participant_members.name as name',
+                        'archery_clubs.name as club_name',
+                        'archery_event_participants.id as participant_id',
+                        'archery_event_participants.user_id as user_id',
+                        'archery_event_participants.event_id'
+                    )
+                        ->where('archery_event_participant_members.id', $participant_member_id)
+                        ->leftJoin('archery_event_participants', 'archery_event_participants.id', 'archery_event_participant_members.archery_event_participant_id')
+                        ->leftJoin('archery_clubs', 'archery_clubs.id', 'archery_event_participants.club_id')
+                        ->first();
+                    $name = $detail_member['name'];
+                    $rank = $elimination_member->elimination_ranked;
+                    $club = $detail_member['club_name'] ? $detail_member['club_name'] : "-";
+                }
+
+
+                $result['name_athlete'][] = $name;
+                $result['rank'][] = $rank;
+                $result['club'][] = $club;
+
+                $category = ArcheryEventCategoryDetail::getCategoryLabelComplete($category_id);
+                if ($category == "") {
+                    throw new BLoCException("Kategori tidak ditemukan");
+                }
+
+                $result['category'][] = $category;
             }
 
+            $html = view('template.score_sheet_elimination', [
+                'peserta1_name' => $result['name_athlete'][0],
+                'peserta2_name' => $result['name_athlete'][1],
+                'peserta1_club' => $result['club'][0],
+                'peserta2_club' => $result['club'][1],
+                'peserta1_rank' => $result['rank'][0],
+                'peserta2_rank' => $result['rank'][1],
+                'peserta1_category' => $result['category'][0],
+                'peserta2_category' => $result['category'][1],
+                "qr" => $base64,
+                "event_name" => $event_name,
+                "location" => $location_event
+            ]);
 
-            $result['name_athlete'][] = $name;
-            $result['rank'][] = $rank;
-            $result['club'][] = $club;
-
-            $category = ArcheryEventCategoryDetail::getCategoryLabelComplete($category_id);
-            if ($category == "") {
-                throw new BLoCException("Kategori tidak ditemukan");
-            }
-
-            $result['category'][] = $category;
+            $all[] = $html;
         }
 
+        $path = 'asset/score_sheet/';
+        $file_name = $path . "scoore_sheet_elimination_" . $elimination_id;
         $mpdf = new Mpdf([
             'margin_left' => 3,
             'margin_right' => 3,
@@ -164,26 +182,16 @@ class BulkDownloadScooresSheetElimination extends Retrieval
             'shrink_tables_to_fit' => 1.4,
             'tempDir' => public_path() . '/tmp/pdf'
         ]);
+        PdfLibrary::setArrayDoc($all)->setFileName($file_name)->savePdf($mpdf, null, null);
+        return [
+            "file_name" => env('APP_HOSTNAME') . $file_name,
+        ];
 
-        $html = view('template.score_sheet_elimination', [
-            'peserta1_name' => $result['name_athlete'][0],
-            'peserta2_name' => $result['name_athlete'][1],
-            'peserta1_club' => $result['club'][0],
-            'peserta2_club' => $result['club'][1],
-            'peserta1_rank' => $result['rank'][0],
-            'peserta2_rank' => $result['rank'][1],
-            'peserta1_category' => $result['category'][0],
-            'peserta2_category' => $result['category'][1],
-            "qr" => $base64,
-            "event_name" => $event_name,
-            "location" => $location_event
-        ]);
+        // $mpdf->WriteHTML($html);
 
-        $mpdf->WriteHTML($html);
-        $path = 'asset/score_sheet/';
-        $full_path = $path . "score_sheet_elimination.pdf";
-        $mpdf->Output(public_path() . "/" . $full_path, "F");
-        return env('APP_HOSTNAME') . $full_path;
+        // $full_path = $path . "score_sheet_elimination.pdf";
+        // $mpdf->Output(public_path() . "/" . $full_path, "F");
+        // return env('APP_HOSTNAME') . $full_path;
     }
 
     private function getTeam($elimination_id, $round, $match, $category_id, $event_name, $location_event)
