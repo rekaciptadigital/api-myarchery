b2444f5e6dad151dd2a8f470670c79b50902a31f
mahfuzon <mahfuzon0@gmail.com>
2022-09-14 09:09:00 +0700

    done create account admin

--- app/BLoC/Web/ManagementAdmin/CreateNewUser.php
+++ app/BLoC/Web/ManagementAdmin/CreateNewUser.php
@@ -2,11 +2,15 @@
 
 namespace App\BLoC\Web\ManagementAdmin;
 
+use App\Jobs\CreateAccountEmailJob;
+use App\Jobs\InvitationEmailJob;
 use App\Models\Admin;
 use App\Models\AdminRole;
+use App\Models\ArcheryEvent;
 use DAI\Utils\Abstracts\Retrieval;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Queue;
 
 class CreateNewUser extends Retrieval
 {
@@ -21,7 +25,9 @@ class CreateNewUser extends Retrieval
         $name = $parameters->get("name");
         $phone_number = $parameters->get("phone_number");
         $role_id = $parameters->get("role_id");
-        $admin = Admin::where("email", $email)->firts();
+        $event_id = $parameters->get("event_id");
+        $event = ArcheryEvent::find($event_id);
+        $admin = Admin::where("email", $email)->first();
         if (!$admin) {
             Validator::make($parameters->all(), [
                 "email" => "unique:admins,email",
@@ -35,16 +41,30 @@ class CreateNewUser extends Retrieval
             $admin->password = $password;
             $admin->phone_number = $phone_number;
             $admin->save();
+
+            Queue::push(new CreateAccountEmailJob([
+                "email" => $admin->email,
+                "name" => $admin->name,
+                "password" => "12345678",
+                "event_name" => $event->event_name
+            ]));
         }
-        
-        $admin_role = AdminRole::where("admin_id", $admin->id)->first();
+
+        $admin_role = AdminRole::where("admin_id", $admin->id)->where("event_id", $event_id)->first();
         if (!$admin_role) {
             $admin_role = new AdminRole;
         }
 
         $admin_role->admin_id = $admin->id;
         $admin_role->role_id = $role_id;
+        $admin_role->event_id = $event_id;
         $admin_role->save();
+
+        Queue::push(new InvitationEmailJob([
+            "event_name" => $event->event_name,
+            "email" => $admin->email,
+            "name" => $admin->name,
+        ]));
     }
 
     protected function validation($parameters)
@@ -53,7 +73,8 @@ class CreateNewUser extends Retrieval
             "email" => "required|email",
             "name" => "required|string",
             "phone_number" => "required|string",
-            "role_id" => "required|integer|exists:roles,id"
+            "role_id" => "required|integer|exists:roles,id",
+            "event_id" => "required|exists:archery_events,id"
         ];
     }
 }
