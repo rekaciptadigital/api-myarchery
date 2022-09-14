04e5a2058c52c7609b66fded0b0a48121ece2f06
mahfuzon <mahfuzon0@gmail.com>
2022-09-14 11:17:22 +0700

    done check admin

--- app/BLoC/Web/ManagementAdmin/CheckAdminExists.php
+++ app/BLoC/Web/ManagementAdmin/CheckAdminExists.php
@@ -3,6 +3,8 @@
 namespace App\BLoC\Web\ManagementAdmin;
 
 use App\Models\Admin;
+use App\Models\AdminRole;
+use App\Models\Role;
 use DAI\Utils\Abstracts\Retrieval;
 
 class CheckAdminExists extends Retrieval
@@ -15,8 +17,20 @@ class CheckAdminExists extends Retrieval
     protected function process($parameters)
     {
         $email = $parameters->get("email");
+        $event_id = $parameters->get("event_id");
 
         $admin = Admin::where("email", $email)->first();
+        if ($admin) {
+            $role_detail = null;
+            $admin_role = AdminRole::where("admin_id", $admin->id)->where("event_id", $event_id)->first();
+            if ($admin_role) {
+                $role = Role::find($admin_role->role_id);
+                if ($role) {
+                    $role_detail = $role;
+                    $admin->role_detail = $role_detail;
+                }
+            }
+        }
         return $admin;
     }
 
@@ -24,6 +38,7 @@ class CheckAdminExists extends Retrieval
     {
         return [
             "email" => "required|email",
+            "event_id" => "required|exists:archery_events,id"
         ];
     }
 }
