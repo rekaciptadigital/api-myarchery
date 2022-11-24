e37f9b0977b9f266661ee213f14c0355fde24a4e
suryadarmasaqti19 <suryadarmasaqti19@gmail.com>
2022-11-24 18:31:15 +0700

    update payment

--- app/Libraries/PaymentGateWay.php
+++ app/Libraries/PaymentGateWay.php
@@ -484,7 +484,7 @@ class PaymentGateWay
         $status = 3;
         if ($result->data->status == 'complete') {
             $status = 1;
-        } else if ($result->data->status == 'processing') {
+        } else if ($result->data->status == 'processing' || $result->data->status == 'waiting_payment') {
             $status = 4;
             // $transaction_log->expired_time = strtotime("+" . env("MIDTRANS_EXPIRE_DURATION_SNAP_TOKEN_ON_MINUTE", 30) . " minutes", time());
         } else if ($result->data->status == 'expired') {
