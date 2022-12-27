a94c31356a84c2d78a9a53161b433d7c4286171a
suryadarmasaqti19 <suryadarmasaqti19@gmail.com>
2022-12-27 11:05:40 +0700

    update scoresheet

--- resources/views/template/score_sheet_elimination.blade.php
+++ resources/views/template/score_sheet_elimination.blade.php
@@ -154,7 +154,7 @@ if($elimination_scoring_type == 1 || $elimination_scoring_type == 0 ){
                             <span
                                 style="border: 1px solid black; padding: 5px;margin-right:5px;">&nbsp;&nbsp;&nbsp;&nbsp;</span>Winner
                         </th>
-                        <th style="width: 10%;background-color: #808080;border-left: none" colspan="{{$colSpanHeader}}">1/4</th>
+                        <th style="width: 10%;background-color: #808080;border-left: none" colspan="{{$colSpanHeader}}">1/5</th>
                     </tr>
                     <tr style="width: 10%;background-color: #808080;border:1px solid black;">
                         <th style="width: 5%;background-color: white;border:none;"></th>
@@ -260,7 +260,7 @@ if($elimination_scoring_type == 1 || $elimination_scoring_type == 0 ){
                             <span
                                 style="border: 1px solid black; padding: 5px;margin-right:5px;">&nbsp;&nbsp;&nbsp;&nbsp;</span>Winner
                         </th>
-                        <th style="width: 10%;background-color: #808080;border-left: none" colspan="{{$colSpanHeader}}">1/4</th>
+                        <th style="width: 10%;background-color: #808080;border-left: none" colspan="{{$colSpanHeader}}">1/5</th>
                     </tr>
                     <tr style="width: 10%;background-color: #808080;border:1px solid black;">
                         <th style="width: 5%;background-color: white;border:none;"></th>
