892ca15b86ec4c02f14d1d11ebc7baa5fa7f803b
mahfuzon <mahfuzon0@gmail.com>
2023-01-20 10:37:50 +0700

    update function insert participant

--- app/Models/ArcheryEventParticipant.php
+++ app/Models/ArcheryEventParticipant.php
@@ -160,7 +160,8 @@ class ArcheryEventParticipant extends Model
     $club_id,
     $day_choice,
     $expired_booking_time = 0,
-    $is_early_bird_payment = 0
+    $is_early_bird_payment = 0,
+    $city_id = 0
   ) {
     return self::create([
       'club_id' => $club_id,
@@ -182,7 +183,8 @@ class ArcheryEventParticipant extends Model
       'event_category_id' => $event_category_detail->id,
       'day_choice' => $day_choice,
       "expired_booking_time" => $expired_booking_time,
-      "is_early_bird_payment" => $is_early_bird_payment
+      "is_early_bird_payment" => $is_early_bird_payment,
+      "city_id" => $city_id
     ]);
   }
 
