d81dbcd8ee85153d2498e4dfa0c6a1e65594a58b
mahfuzon <mahfuzon0@gmail.com>
2023-02-07 15:43:02 +0700

    fix bug

--- app/Models/ArcheryEventParticipantMember.php
+++ app/Models/ArcheryEventParticipantMember.php
@@ -23,6 +23,7 @@ class ArcheryEventParticipantMember extends Model
             }
 
             $member->have_coint_tost = 0;
+            $member->rank_can_change = null;
             $member->save();
         }
     }
