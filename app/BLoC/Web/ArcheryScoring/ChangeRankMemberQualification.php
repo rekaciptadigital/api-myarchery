f9612506d6724ef2c91eb418027e8044a31bafa4
mahfuzon <mahfuzon0@gmail.com>
2023-02-09 09:58:39 +0700

    1. tambahin validasi jika sudah ada eliminasi 2. switch rank member

--- app/BLoC/Web/ArcheryScoring/ChangeRankMemberQualification.php
+++ app/BLoC/Web/ArcheryScoring/ChangeRankMemberQualification.php
@@ -2,6 +2,8 @@
 
 namespace App\BLoC\Web\ArcheryScoring;
 
+use App\Models\ArcheryEventElimination;
+use App\Models\ArcheryEventParticipant;
 use App\Models\ArcheryEventParticipantMember;
 use DAI\Utils\Abstracts\Transactional;
 use DAI\Utils\Exceptions\BLoCException;
@@ -24,20 +26,38 @@ class ChangeRankMemberQualification extends Transactional
             throw new BLoCException("member not have coint tost");
         }
 
+        $participant = ArcheryEventParticipant::find($member->archery_event_participant_id);
+        if (!$participant) {
+            throw new BLoCException("participant not found");
+        }
+
+        $event_elimination = ArcheryEventElimination::where("event_category_id", $participant->event_category_id)->first();
+        if ($event_elimination) {
+            throw new BLoCException("eliminasi sudah ditentukan");
+        }
+
         if ($member->rank_can_change == null) {
-            throw new BLoCException("rank can join null");
+            throw new BLoCException("rank can change null");
         }
 
         if (!in_array($rank, json_decode($member->rank_can_change))) {
             throw new BLoCException("invalid rank input");
         }
 
-        $member->have_coint_tost = 2;
-        $member->save();
-
         $member_rank = MemberRank::where("member_id", $member->id)->first();
         if (!$member_rank) {
             throw new BLoCException("member rank not found");
+        }
+
+        $list_member_rank = MemberRank::where("category_id", $participant->event_category_id)->get();
+        foreach ($list_member_rank as $mr) {
+            if ($mr->id == $member_rank->id) {
+                continue;
+            }
+
+            if ($mr->rank == $rank) {
+                $mr->rank = $member_rank->rank;
+            }
         }
 
         $member_rank->rank = $rank;
