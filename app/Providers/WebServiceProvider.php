8c4a422be738c842b2a3b3950ba882953ef1ceb5
mahfuzon <mahfuzon0@gmail.com>
2023-02-13 11:25:38 +0700

    coba fix 4

--- app/Providers/WebServiceProvider.php
+++ app/Providers/WebServiceProvider.php
@@ -52,6 +52,7 @@ use App\BLoC\General\CategoryDetail\GetListCategoryByEventId;
 use App\BLoC\General\Event\GetDetailEventByIdGeneral;
 use App\BLoC\General\Event\GetDetailEventBySlugV2;
 use App\BLoC\General\GetEventClubRanked;
+use App\BloC\General\InsertAllCategoryEventToSeries;
 use App\BLoC\General\QandA\GetQandAByEventId;
 use App\BLoC\Web\AdminAuth\UpdateProfile;
 use App\BLoC\Web\ArcheryScoring\AddParticipantMemberScore;
@@ -247,7 +248,7 @@ class WebServiceProvider extends ServiceProvider
         $this->registerService("addArcheryCategory", AddArcheryCategory::class);
         $this->registerService("editArcheryCategory", EditArcheryCategory::class);
         $this->registerService("getArcheryCategory", GetArcheryCategory::class);
-        $this->registerService("addAllCategoryEventToSeries", AddAllCategoryEventToSeries::class);
+        $this->registerService("insertAllCategoryEventToSeries", InsertAllCategoryEventToSeries::class);
 
         // ================================ queen series 4 =========================================
         $this->registerService("reportRankQualification", ReportRankQualification::class);
