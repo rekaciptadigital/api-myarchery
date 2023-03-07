3ebedaa77ff95f14c5fd3ddf351d2ee926f43aec
mahfuzon <mahfuzon0@gmail.com>
2023-03-07 09:06:21 +0700

    done laporan irate

--- resources/views/reports/event_selection/elimination.blade.php
+++ resources/views/reports/event_selection/elimination.blade.php
@@ -29,8 +29,12 @@
             <tbody>
                 <tr style="height: 20px;">
                     <td style="width: 2%; height: 50px;" rowspan="2"></td>
-                    <td style="width: 10%; height: 50px;" rowspan="2">{!! $logo_event !!}</td>
-                    <td style="width: 10%; height: 50px;" rowspan="2">{!! $logo_archery !!}</td>
+                    <td style="width: 10%; height: 50px;" rowspan="2">
+                        <img src="{{ $logo_event }}" alt="" width="70%">
+                    </td>
+                    <td style="width: 10%; height: 50px;" rowspan="2">
+                        {!! $logo_archery !!}
+                    </td>
                     <td style="width: 42%; height: 30px; ">
                         <p style="text-align: left; font-size: 14pt; font-family: helvetica;">
                             <strong><span style="font-size: 20px;">{{ $event_name_report }}</span></strong> <br />
@@ -43,68 +47,87 @@
         </table>
         <h2 style="text-align: center; font-size: 24px;">Elimination</h2>
         @foreach ($datas as $key => $elimination)
-        <p style="font-size: 18px; text-align: right; margin-bottom: 5px;"><b>After {{ $elimination['total_arrow'] }} arrows</b></p>
-        <table class="table" style="width:100%;border: 1px solid black; border-collapse: collapse; margin-bottom: 25px;">
-            <thead>
-                <tr>
-                    <th style="text-align: center;border: 1px solid black; font-size: 20px; background-color: lightgray;" colspan="11">{{ $elimination['category'] }}</th>
-                </tr>
-            </thead>
-            <tbody style="font-size: 18px;">
-                <tr style="border: 1px solid black; background-color: lightgray;">
-                    <th style="text-align: center;border: 1px solid black; ">
-                        <strong>Pos.</strong>
-                    </th>
-                    <th style="text-align: center;border: 1px solid black; ">
-                        <strong>Budrest</strong>
-                    </th>
-                    <th style="text-align: center;border: 1px solid black; ">
-                        <strong>Athlete</strong>
-                    </th>
-                    <th style="text-align: center;border: 1px solid black; ">
-                        <strong>Club</strong>
-                    </th>
-                    <th style="text-align: center;border: 1px solid black;">
-                        <strong>Eli-1</strong>
-                    </th>
-                    <th style="text-align: center; border: 1px solid black;">
-                        <strong>Eli-2</strong>
-                    </th>
-                    <th style="text-align: center;border: 1px solid black;">
-                        <strong>Eli-3</strong>
-                    </th>
-                    <th style="text-align: center; border: 1px solid black;">
-                        <strong>Eli-4</strong>
-                    </th>
-                    <th style="text-align: center;border: 1px solid black;">
-                        <strong>Eli-5</strong>
-                    </th>
-                    <th style="text-align: center; border: 1px solid black;">
-                        <strong>Total</strong>
-                    </th>
-                    <th style="text-align: center; border: 1px solid black;">
-                        <strong>IRAT</strong>
-                    </th>
-                </tr>
-                @php($i = 1)
-                @foreach ($elimination['data'] as $key => $data)
-                    <tr style="border: 1px solid black;">
-                        <td style="text-align: center;border: 1px solid black;"> {{ $i }}</td>
-                        <td style="text-align: center;border: 1px solid black;"> {{ $data['member']['bud_rest_number'] }} {{ $data['member']['target_face'] }} </td>
-                        <td style="text-align: center;border: 1px solid black;"> {{ $data['member'] ? strtoupper($data['member']['name']) : '-' }}</td>
-                        <td style="text-align: center;border: 1px solid black;"> {{ $data['club_name'] ? strtoupper($data['club_name']) : '-' }}</td>
-                        <td style="text-align: center;border: 1px solid black;"> {{ $data['sessions']['1']['total'] }}</td>
-                        <td style="text-align: center;border: 1px solid black;"> {{ $data['sessions']['2']['total'] }}</td>
-                        <td style="text-align: center;border: 1px solid black;"> {{ $data['sessions']['3']['total'] }}</td>
-                        <td style="text-align: center;border: 1px solid black;"> {{ $data['sessions']['4']['total'] }}</td>
-                        <td style="text-align: center;border: 1px solid black;"> {{ $data['sessions']['5']['total'] }}</td>
-                        <td style="text-align: center;border: 1px solid black;"> {{ $data['total'] }}</td>
-                        <td style="text-align: center;border: 1px solid black;"> {{ $data['total_irat'] }}</td>
+            <p style="font-size: 18px; text-align: right; margin-bottom: 5px;"><b>After
+                    {{ $elimination['total_arrow'] }} arrows</b>
+            </p>
+            <table class="table"
+                style="width:100%;border: 1px solid black; border-collapse: collapse; margin-bottom: 25px;">
+                <thead>
+                    <tr>
+                        <th style="text-align: center;border: 1px solid black; font-size: 20px; background-color: lightgray;"
+                            colspan="11">
+                            {{ $elimination['category'] }}
+                        </th>
                     </tr>
-                    @php($i++)
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody style="font-size: 18px;">
+                    <tr style="border: 1px solid black; background-color: lightgray;">
+                        <th style="text-align: center;border: 1px solid black; ">
+                            <strong>Pos.</strong>
+                        </th>
+                        <th style="text-align: center;border: 1px solid black; ">
+                            <strong>Budrest</strong>
+                        </th>
+                        <th style="text-align: center;border: 1px solid black; ">
+                            <strong>Athlete</strong>
+                        </th>
+                        <th style="text-align: center;border: 1px solid black; ">
+                            <strong>Club</strong>
+                        </th>
+                        <th style="text-align: center;border: 1px solid black;">
+                            <strong>Eli-1</strong>
+                        </th>
+                        <th style="text-align: center; border: 1px solid black;">
+                            <strong>Eli-2</strong>
+                        </th>
+                        <th style="text-align: center;border: 1px solid black;">
+                            <strong>Eli-3</strong>
+                        </th>
+                        <th style="text-align: center; border: 1px solid black;">
+                            <strong>Eli-4</strong>
+                        </th>
+                        <th style="text-align: center;border: 1px solid black;">
+                            <strong>Eli-5</strong>
+                        </th>
+                        <th style="text-align: center; border: 1px solid black;">
+                            <strong>Total</strong>
+                        </th>
+                        <th style="text-align: center; border: 1px solid black;">
+                            <strong>IRAT</strong>
+                        </th>
+                    </tr>
+                    @php($i = 1)
+                    @foreach ($elimination['data'] as $key => $data)
+                        <tr style="border: 1px solid black;">
+                            <td style="text-align: center;border: 1px solid black;"> {{ $i }}</td>
+                            <td style="text-align: center;border: 1px solid black;">
+                                {{ $data['member']['bud_rest_number'] }} {{ $data['member']['target_face'] }} </td>
+                            <td style="text-align: center;border: 1px solid black;">
+                                {{ $data['member'] ? strtoupper($data['member']['name']) : '-' }}</td>
+                            <td style="text-align: center;border: 1px solid black;">
+                                {{ $data['club_name'] ? strtoupper($data['club_name']) : '-' }}</td>
+                            <td style="text-align: center;border: 1px solid black;">
+                                {{ isset($data['sessions']) && isset($data['sessions']['1']) && isset($data['sessions']['1']['total']) ? $data['sessions']['1']['total'] : 0 }}
+                            </td>
+                            <td style="text-align: center;border: 1px solid black;">
+                                {{ isset($data['sessions']) && isset($data['sessions']['2']) && isset($data['sessions']['2']['total']) ? $data['sessions']['2']['total'] : 0 }}
+                            </td>
+                            <td style="text-align: center;border: 1px solid black;">
+                                {{ isset($data['sessions']) && isset($data['sessions']['3']) && isset($data['sessions']['3']['total']) ? $data['sessions']['3']['total'] : 0 }}
+                            </td>
+                            <td style="text-align: center;border: 1px solid black;">
+                                {{ isset($data['sessions']) && isset($data['sessions']['4']) && isset($data['sessions']['4']['total']) ? $data['sessions']['4']['total'] : 0 }}
+                            </td>
+                            <td style="text-align: center;border: 1px solid black;">
+                                {{ isset($data['sessions']) && isset($data['sessions']['5']) && isset($data['sessions']['5']['total']) ? $data['sessions']['5']['total'] : 0 }}
+                            </td>
+                            <td style="text-align: center;border: 1px solid black;"> {{ $data['total'] }}</td>
+                            <td style="text-align: center;border: 1px solid black;"> {{ $data['total_irat'] }}</td>
+                        </tr>
+                        @php($i++)
+                    @endforeach
+                </tbody>
+            </table>
         @endforeach
     </div>
 </body>
