7cab64fbe5b66bc04c8c997faa80dd46acba3feb
mahfuzon <mahfuzon0@gmail.com>
2023-04-13 15:34:28 +0700

    perbaiki endpoin set dan get config aturan pertandingan

--- app/BLoC/Web/ConfigRambahanArrow/GetConfig.php
+++ app/BLoC/Web/ConfigRambahanArrow/GetConfig.php
@@ -3,6 +3,7 @@
 namespace App\BLoC\Web\ConfigRambahanArrow;
 
 use App\Models\ArcheryEvent;
+use App\Models\ArcheryEventCategoryDetail;
 use App\Models\ArcheryMasterAgeCategory;
 use App\Models\ArcheryMasterCompetitionCategory;
 use App\Models\ArcheryMasterDistanceCategory;
@@ -27,8 +28,6 @@ class GetConfig extends Transactional
         // tangkap semua params
         $event_id = $parameters->get('event_id');
 
-
-
         $event = ArcheryEvent::find($event_id);
 
         // cek pemilik event
@@ -38,21 +37,14 @@ class GetConfig extends Transactional
 
         $response = [];
         $active_setting_1 = 0;
-        $implement_all_1 = 1;
-        $session_1 = 2;
-        $arrow_1 = 6;
-        $rambahan_1 = 6;
-        $shoot_rule_1 = null;
-        $response["event_id"] = $event->id;
+        $shoot_rule_1 = [];
         $config = ConfigArrowRambahan::where("event_id", $event_id)->first();
 
         if ($config) {
             $active_setting_1 = 1;
             if ($config->type == 2) {
-                $implement_all_1 = 0;
                 $list_special_config = [];
                 $list_category_config = CategoryConfig::where("config_arrow_rambahan_id", $config->id)->get(); // tangkap semua config list_category khusus 
-
                 foreach ($list_category_config as $category_config) {
                     $categories = [];
                     $list_category_2 = CategoryConfigMappingArrowRambahan::where("config_category_id", $category_config->id)->get();
@@ -81,32 +73,17 @@ class GetConfig extends Transactional
                 }
 
                 $shoot_rule_1 = $list_special_config;
-            } else {
-                $session_1 = $config->session;
-                $arrow_1 = $config->arrow;
-                $rambahan_1 = $config->rambahan;
             }
         }
 
+        $response["event_id"] = $event->id;
         $response["active_setting"] = $active_setting_1;
-        $response["implement_all"] = $implement_all_1;
-        if ($implement_all_1 == 1) {
-            $response["session"] = $session_1;
-            $response["child_bow"] = $arrow_1;
-            $response["rambahan"] = $rambahan_1;
-            $response["shoot_rule"] = [
-                "session" => 0,
-                "rambahan" => 0,
-                "child_bow" => 0,
-                "category" => []
-            ];
-        }
-
-        if ($implement_all_1 != 1) {
+        if ($active_setting_1 == 1) {
             $response["shoot_rule"] = $shoot_rule_1;
         }
 
-        // return "ok";
+        $response["shoot_rule"] = $shoot_rule_1;
+
         return $response;
     }
 
